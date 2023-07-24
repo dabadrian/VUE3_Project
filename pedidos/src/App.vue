6795043d6946e4b5b25134033765908450118264
Daniel Blanco <74727652+dabadrian@users.noreply.github.com>
2023-07-23 23:56:04 -0400

    App

---
+++ pedidos/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo"></a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/customer">Clientes</router-link></li>
+          <li><router-link to="/product">Productos</router-link></li>
+          <li><router-link to="/order">Preventas</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li v-if="$store.getters.isLogin"><a @click="logout()">Salir</a></li>
+          <li v-if="!$store.getters.isLogin"><router-link to="/login">Login</router-link></li>        
+        </ul>
+      </div>
+    </nav>
+
+    <ul class="sidenav" id="mobile-demo">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/customer">Clientes</router-link></li>
+      <li><router-link to="/product">Productos</router-link></li>
+      <li><router-link to="/order">preventas</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li v-if="$store.getters.isLogin"><a @click="logout()">Salir</a></li>
+      <li v-if="!$store.getters.isLogin"><router-link to="/login">Login</router-link></li> 
+    </ul>
+    <div>es login {{ $store.getters.isLogin }} {{ $store.state }}</div>
+  <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+
+
+</style>
+
+<script>
+
+
+
+export default {
+  name: 'AppView',
+  data(){
+    return {
+      
+    }
+  },
+  methods:{
+    logout(){
+      this.$store.dispatch("doLogout");
+      this.$router.push("/login");
+
+    }
+  },
+  created() {
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems, {});
+
+    });
+  }
+}
+</script>
---
+++ pedidos/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h5>Inicio</h5>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
